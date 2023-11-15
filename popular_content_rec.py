4e46a4c92c2b4a5a8c9e653f2ca4141e94ca6088
Rizki Maula <107558564+RizkiMaula65@users.noreply.github.com>
2023-11-15 08:27:28 +0700

    Update popular_content_rec.py
    fixed writing in program code

--- popular_content_rec.py
+++ popular_content_rec.py
@@ -1,10 +1,7 @@
-#import library
 import pandas as pd
-
 import warnings
 warnings.filterwarnings('ignore')
 
-#Preprocessing rating data
 def preprocess_rating(rating):
     rating['user_id'] = rating['user_id'].astype(int)
     rating['fiction_id'] = rating['fiction_id'].astype(int)
@@ -13,7 +10,6 @@ def preprocess_rating(rating):
     rating['rating'] = rating['rating'].astype(int)
     return rating
 
-#Preprocessing rating data
 def preprocess_fiction(fiction):
     fiction['fiction_id'] = fiction['fiction_id'].astype(int)
     fiction['overview'] = fiction['overview'].astype(str)
@@ -23,7 +19,6 @@ def preprocess_fiction(fiction):
     fiction['chapter'] = fiction['chapter'].astype(str)
     return fiction
 
-#menghitung count_rating, mean_rating, like, click, dan popularitas berdasarkan fiction_id
 def calculate_rating(rating):
     fiction_recs = rating.groupby("fiction_id").rating.agg(['count','mean'])
     fiction_recs['click'] = rating.groupby("fiction_id").click.agg(['count'])
@@ -32,26 +27,20 @@ def calculate_rating(rating):
     return fiction_recs
 
 def request_popular_content(rating_dir='./json/rating.json', fiction_dir='./json/fiction.json'):
-
-    #memuat data rating dan fiction_metadata
     rating = pd.read_json(rating_dir)
     fiction_metadata = pd.read_json(fiction_dir)
 
-    #membuat dataframe fiction
+
     fiction = fiction_metadata[['fiction_id', 'title', 'overview', 'language', 'tags', 'genres', 'chapter']]
     fiction['title_mod'] = fiction["title"].str.replace("[^a-zA-Z0-9 ]", "").str.lower().str.replace("\s+", " ", regex=True)
     
-    #preprocessing data rating dan fiction_metadata
+
     rating = preprocess_rating(rating)
     fiction = preprocess_fiction(fiction)
 
-    #membuat dataframe fiction_recs untuk menghitung rating count, rating mean, click, like, dan popularity
     fiction_recs = calculate_rating(rating)
-
-    #merge dataframe fiction dengan fiction_recs
     fiction = fiction.merge(fiction_recs, how='inner', on='fiction_id')
-
     fiction_sorted = fiction.sort_values(by='popularity', ascending=False)
     return fiction_sorted[['fiction_id']].head(20).to_json()
 
-print(request_popular_content())
+request_popular_content()
