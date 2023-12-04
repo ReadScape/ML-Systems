292a7d1148d08e4e2ba27d678162851d5ebabad9
Rizki Maula <107558564+RizkiMaula65@users.noreply.github.com>
2023-12-04 14:52:53 +0700

    Update and rename recommendation_content.py to search_recommendation_content.py
    Make adjustments to the program code according to data sources from the cloud computing team. In this program code, there is also a change in the rating from initially containing interaction data to calculated rating data.

--- search_recommendation_content.py
+++ search_recommendation_content.py
@@ -3,7 +3,7 @@ recommendation_content.py: Recommends fiction based on content similarity with t
 
 Args:
     title (str): The title of the fanfiction to be recommended.
-    rating_dir (str): The directory of the rating data.
+    rating_dir (str): The directory for rating data. Default is 'https://readscape.live/calculatedrating'.
     fiction_dir (str): The directory of the fiction data.
 
 Returns:
@@ -14,9 +14,7 @@ import string
 import re
 import pandas as pd
 import numpy as np
-import sys
 import requests
-import io
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -35,11 +33,13 @@ def preprocess_rating(rating):
     Returns:
         pd.DataFrame: The preprocessed rating data.
     """
-    rating['user_id'] = rating['user_id'].astype(int)
-    rating['fiction_id'] = rating['fiction_id'].astype(int)
+    rating['fiction_id'] = rating['fiction_id'].astype(str)
+    rating['count'] = rating['count'].astype(int)
+    rating['mean'] = rating['mean'].astype(float)
     rating['click'] = rating['click'].astype(int)
-    rating['like'] = rating['like'].astype(int)
-    rating['rating'] = rating['rating'].astype(int)
+    rating['love'] = rating['love'].astype(int)
+    rating['popularity'] = rating['popularity'].astype(int)
+    rating['weighted_mean'] = rating['weighted_mean'].astype(float)
     return rating
 
 def preprocess_fiction(fiction):
@@ -52,61 +52,24 @@ def preprocess_fiction(fiction):
     Returns:
         pd.DataFrame: The preprocessed fiction data.
     """
-    fiction['fiction_id'] = fiction['fiction_id'].astype(int)
-    fiction['overview'] = fiction['overview'].astype(str)
-    fiction['language'] = fiction['language'].astype(str)
+    fiction['fiction_id'] = fiction['fiction_id'].astype(str)
+    fiction['synopsis'] = fiction['synopsis'].astype(str)
     fiction['tags'] = fiction['tags'].astype(str)
-    fiction['genres'] = fiction['genres'].astype(str)
-    fiction['chapter'] = fiction['chapter'].astype(str)
+    fiction['chapters'] = fiction['chapters'].astype(str)
     return fiction
 
-def calculate_rating(rating):
-    """
-    Calculates the count_rating, mean_rating, like, click, and popularity based on fiction_id.
-
-    Args:
-        rating (pd.DataFrame): The rating data.
-
-    Returns:
-        pd.DataFrame: The calculated ratings.
-    """
-    fiction_recs = rating.groupby("fiction_id").rating.agg(['count','mean'])
-    fiction_recs['click'] = rating.groupby("fiction_id").click.agg(['count'])
-    fiction_recs['like'] = rating.groupby("fiction_id").like.agg(['count'])
-    fiction_recs['popularity'] = fiction_recs['click'] + fiction_recs['like']
-    return fiction_recs
-
-def calculate_weighted_mean(fiction, fiction_recs):
-    """
-    Calculates the weighted mean based on the count_rating, mean_rating, and popularity.
-
-    Args:
-        fiction (pd.DataFrame): The fiction data.
-        fiction_recs (pd.DataFrame): The calculated ratings.
-
-    Returns:
-        pd.Series: The weighted mean values.
-    """
-    r = fiction['mean']
-    v = fiction['count']
-    m = fiction['count'].quantile(0.8)
-    C = fiction['mean'].mean()
-    fiction_recs['weighted_mean'] = (r * v + C * m) / (v + m)
-    return fiction_recs['weighted_mean']
-
-def preprocess_rating_pred(fiction, fiction_recs):
+def preprocess_rating_pred(fiction):
     """
     Preprocesses the rating predictions by scaling and calculating the score.
 
     Args:
         fiction (pd.DataFrame): The fiction data.
-        fiction_recs (pd.DataFrame): The calculated ratings.
 
     Returns:
         pd.DataFrame: The preprocessed rating predictions.
     """
     mm_scaler = MinMaxScaler()
-    scaled = mm_scaler.fit_transform(fiction_recs[['popularity', 'weighted_mean']])
+    scaled = mm_scaler.fit_transform(fiction[['popularity', 'weighted_mean']])
     rating_pred = pd.DataFrame(scaled, columns=['popularity', 'weighted_mean'])
     rating_pred.index = fiction['fiction_id']
     rating_pred['score'] = rating_pred['weighted_mean'] * 0.4 + rating_pred['popularity'].astype('float64') * 0.6
@@ -139,12 +102,10 @@ def preprocess_content(content_df):
     Returns:
         pd.DataFrame: The preprocessed content dataframe.
     """
-    content_df['overview'] = content_df['overview'].apply(preprocess_text)
-    content_df['language'] = content_df['language'].apply(preprocess_text)
+    content_df['synopsis'] = content_df['synopsis'].apply(preprocess_text)
     content_df['tags'] = content_df['tags'].apply(preprocess_text)
-    content_df['genres'] = content_df['genres'].apply(preprocess_text)
     content_df['atribute'] = ''
-    content_df['atribute'] = content_df[content_df.columns[1:]].apply(lambda x: ' '.join(x), axis=1)
+    content_df['atribute'] = content_df[content_df.columns[1:]].apply(lambda x: ' '.join(map(str, x)), axis=1)
     content_df.set_index(['fiction_id','title'], inplace=True)
     content_df = content_df[['atribute']]
     return content_df
@@ -195,45 +156,43 @@ def predict(title, fiction, content_df, similarity_weight, top_n):
     content_df_sorted.set_index('title', inplace=True)
     merged_df = content_df_sorted.merge(fiction, on='fiction_id', how='left')
     
-    return merged_df[['fiction_id']]
+    return merged_df[['fiction_id','title','tags','synopsis', 'final_score']]
 
-def cbf_recommendation_with_prediction(title, rating_dir='https://docs.google.com/spreadsheets/d/e/2PACX-1vQwAbiu8ldvpGWuIZtrx_QFzFZ_SvXAxLdbhmNg5lyxiflfNmzm94Ie3mJioEumkslTXOP_d-WuwNfX/pub?output=csv', fiction_dir='https://docs.google.com/spreadsheets/d/e/2PACX-1vT2u8napMK0lNviIL48m4jUiqtLezC2KlI61HWr5ekunHYVYYbNJKfP_4PptUgr7ZYwz_z1ozC9mfzh/pub?output=csv'):
+def search_recommendation_content(title, rating_dir='https://readscape.live/calculatedrating', fiction_dir='https://readscape.live/fiction'):
     """
-    Generates content-based recommendations with prediction.
+    Searches for book recommendations based on content similarity.
 
     Args:
-        title (str): The title of the fanfiction to be recommended.
-        rating_dir (str): The directory of the rating data.
-        fiction_dir (str): The directory of the fiction data.
+        title (str): The input title.
+        rating_dir (str): The directory for rating data. Default is 'https://readscape.live/calculatedrating'.
+        fiction_dir (str): The directory for fiction data. Default is 'https://readscape.live/fiction'.
 
     Returns:
-        json: The top 10 recommended fanfiction titles.
+        pd.DataFrame: The top recommended fiction.
     """
-    rating_dir = requests.get(rating_dir).content
-    fiction_dir = requests.get(fiction_dir).content
+    rating_dir = requests.get(rating_dir).json()
+    fiction_dir = requests.get(fiction_dir).json()
 
-    rating = pd.read_csv(io.StringIO(rating_dir.decode('utf-8'))) #need adjustment --> change to json
-    fiction_metadata = pd.read_csv(io.StringIO(fiction_dir.decode('utf-8'))) #need adjustment --> change to json
+    rating = pd.DataFrame(rating_dir['data'])
+    fiction = pd.DataFrame(fiction_dir['data'])
 
-    fiction = fiction_metadata[['fiction_id', 'title', 'overview', 'language', 'tags', 'genres', 'chapter']]
     fiction['title_mod'] = fiction["title"].str.replace("[^a-zA-Z0-9 ]", "").str.lower().str.replace("\s+", " ", regex=True)
 
     rating = preprocess_rating(rating)
     fiction = preprocess_fiction(fiction)
 
-    fiction_recs = calculate_rating(rating)
-    fiction = fiction.merge(fiction_recs, how='inner', on='fiction_id')
-    fiction['weighted_mean'] = calculate_weighted_mean(fiction, fiction_recs)
-    rating_pred_sorted = preprocess_rating_pred(fiction,fiction_recs)
+    fiction = fiction.merge(rating, how='inner', on='fiction_id')
+    rating_pred_sorted = preprocess_rating_pred(fiction)
 
-    content_df = fiction[['fiction_id', 'title', 'overview', 'language', 'tags', 'genres', 'chapter']]
+    content_df = fiction[['fiction_id', 'title', 'synopsis', 'tags', 'chapters']]
     content_df = preprocess_content(content_df)   
     content_df = rating_pred_sorted[:10000].merge(content_df, left_index=True, right_index=True, how='left')
     content_df = content_df.reset_index()
 
     title = similar_title(title, fiction)
     recommendation = predict(title, fiction, content_df, 0.9, 10)
+    
     return recommendation.to_json()
 
-data = sys.argv[1]
-cbf_recommendation_with_prediction(data)
+#test for title 'golden'
+search_recommendation_content('golden')
