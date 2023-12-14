58bd1a790f592ea0f4f0f389f9fb63cc0c989198
dvaalmeyda <divaalmeida99@gmail.com>
2023-12-15 01:34:38 +0700

    ubah load_data.py, baca data dari json

--- Fanfiction_RecSys/load_data.py
+++ Fanfiction_RecSys/load_data.py
@@ -6,23 +6,38 @@ warnings.filterwarnings('ignore')
 
 def load_data(url_rating, url_fiction):
     """
-    Load data from the given URLs and return the rating and fiction metadata.
+    Load data from the given URLs and convert them into DataFrames.
 
     Parameters:
-    url_rating (str): The URL of the rating dataset.
-    url_fiction (str): The URL of the fiction dataset.
+    url_rating (str): The URL to fetch the rating data.
+    url_fiction (str): The URL to fetch the fiction data.
 
     Returns:
-    rating (pd.DataFrame): The rating data.
-    fiction_metadata (pd.DataFrame): The fiction metadata with selected columns.
+    rating_data (DataFrame): The rating data as a DataFrame.
+    fiction_data (DataFrame): The fiction data as a DataFrame.
     """
+    # Load data
+    rating_content = requests.get(url_rating).json()
+    fiction_content = requests.get(url_fiction).json()
 
-    # Load data from the given URLs
-    rating_content = requests.get(url_rating).content
-    fiction_content = requests.get(url_fiction).content
+    # Convert to DataFrame
+    rating_data = pd.DataFrame(rating_content['data'])
+    fiction_data = pd.DataFrame(fiction_content['data'])
 
-    # Convert the content to pandas DataFrame
-    rating = pd.read_csv(io.StringIO(rating_content.decode('utf-8')))
-    fiction_metadata = pd.read_csv(io.StringIO(fiction_content.decode('utf-8')))
+    rating_data = rating_data[['user_id', 'fiction_id', 'click', 'love', 'rating']]
+    fiction_data = fiction_data[['fiction_id', 'title', 'created_at', 'tags']]
 
-    return rating, fiction_metadata[['fiction_id', 'title', 'release_date', 'genres']] # Columns to used
+    return rating_data, fiction_data
+
+# Check
+#url_rating = "https://readscape.live/fiction_ratings"
+#url_fiction = "https://readscape.live/fiction"
+
+#rating_data, fiction_data = load_data(url_rating, url_fiction)
+
+# Display the loaded data
+#print("Rating Data:")
+#print(rating_data)
+
+#print("\nFiction Metadata:")
+#print(fiction_data)
