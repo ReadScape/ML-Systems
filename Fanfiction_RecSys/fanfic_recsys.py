6fca325210e1e28ea594f1bb8770251261cebc90
dvaalmeyda <divaalmeida99@gmail.com>
2023-12-15 01:42:34 +0700

    penyesuaian sistem request fanfic recsys for cold start

--- Fanfiction_RecSys/fanfic_recsys.py
+++ Fanfiction_RecSys/fanfic_recsys.py
@@ -25,18 +25,34 @@ def request_fanfic_recommendation(url_rating, url_fiction, genre=None):
     combined_data = calculate_engagement(rating_recsys)
 
     # Merge dataframes based on 'fiction_id'
-    fanfic_recsys = pd.merge(fiction_recsys, rating_recsys, on='fiction_id', how='inner')
-    fanfic_recsys = pd.merge(fanfic_recsys, combined_data[['fiction_id', 'Total']], on='fiction_id', how='left')
+    fanfic_recsys = pd.merge(fiction_recsys, rating_recsys, on='fiction_id', how='outer')
+    fanfic_recsys = pd.merge(fanfic_recsys, combined_data[['fiction_id', 'Total']], on='fiction_id', how='inner')
 
     # Remove duplicates for 'fiction_id'
     fanfic_recsys = fanfic_recsys.drop_duplicates(subset=['fiction_id'])
 
     # Filter by genre and sort by Total
     if genre:
-        filtered_data = fanfic_recsys[fanfic_recsys['genres'].str.contains(genre, case=False, na=False)]
+        filtered_data = fanfic_recsys[fanfic_recsys['tags'].str.contains(genre, case=False, na=False)]
         filtered_data = filtered_data.sort_values(by='Total', ascending=False)
     else:
         filtered_data = fanfic_recsys.sort_values(by='Total', ascending=False)
 
+    # Display the top recommendations with click = NaN
+    #pd.set_option('display.max_rows', None)
+    #pd.set_option('display.max_columns', None)
+    #pd.set_option('display.width', None)
+    #pd.set_option('display.max_colwidth', None)
+
+    #print(filtered_data[['user_id', 'fiction_id', 'title', 'click', 'Total']].head(20))
+
     # Display the top recommendations in JSON format
     print(filtered_data[['fiction_id']].head(20).to_json())
+
+
+# Check
+#url_rating = "https://readscape.live/fiction_ratings"
+#url_fiction = 'https://readscape.live/fiction'
+#genre = None  # set the desired genre: 'fantasy', 'romance', 'adventure', etc.
+
+#request_fanfic_recommendation(url_rating, url_fiction, genre)
