f0a7edae2a4d1a4c6c9206e4455f6f10118277ff
dvaalmeyda <divaalmeida99@gmail.com>
2023-12-15 01:38:56 +0700

    penyesuaian prep_data.py

--- Fanfiction_RecSys/prep_data.py
+++ Fanfiction_RecSys/prep_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from load_data import load_data
 
 def preprocess_data(rating, fiction):
     """
@@ -15,15 +16,28 @@ def preprocess_data(rating, fiction):
     if 'user_id' not in rating.columns:
         raise ValueError("The 'user_id' column is not present in the rating DataFrame.")
 
-    rating['user_id'] = rating['user_id'].astype(int)
-    rating['fiction_id'] = rating['fiction_id'].astype(int)
+    rating['user_id'] = rating['user_id'].astype(str)
+    rating['fiction_id'] = rating['fiction_id'].astype(str)
     rating['click'] = rating['click'].astype(int)
-    rating['like'] = rating['like'].astype(int)
+    rating['love'] = rating['love'].astype(int)
     rating['rating'] = rating['rating'].astype(int)
 
-    fiction['fiction_id'] = fiction['fiction_id'].astype(int)
+    fiction['fiction_id'] = fiction['fiction_id'].astype(str)
     fiction['title'] = fiction['title'].astype(str)
-    fiction['release_date'] = pd.to_datetime(fiction['release_date'])
-    fiction['genres'] = fiction['genres'].astype(str)
+    fiction['created_at'] = pd.to_datetime(fiction['created_at'])
+    fiction['tags'] = fiction['tags'].astype(str)
 
     return rating, fiction
+
+# Check
+#rating, fiction = load_data(
+#    url_rating = "https://readscape.live/fiction_ratings",
+#    url_fiction = "https://readscape.live/fiction"
+#)
+
+#rating, fiction = preprocess_data(rating, fiction)
+
+# Merge data
+#fanfic_data = fiction.merge(rating, how='outer', on='fiction_id')
+
+#print(fanfic_data)
